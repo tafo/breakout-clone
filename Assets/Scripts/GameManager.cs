a2f1252893dcacd6498eccde3388223325292317
tafo <tayfunyirdem@gmail.com>
2020-11-20 10:54:20 +0300

    Bricks are designed. Levels are cleared.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,206 +1,215 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+namespace Assets.Scripts
 {
-    public GameObject ballPrefab;
-    public GameObject playerPrefab;
-
-    public Text scoreText;
-    public Text ballsText;
-    public Text levelText;
-    public Text highScoreText;
-
-    public GameObject panelMenu;
-    public GameObject panelPlay;
-    public GameObject panelLevelCompleted;
-    public GameObject panelGameOver;
-
-    public GameObject[] levels;
-
-    public enum State { MENU, INIT, PLAY, LEVELCOMPLETED, LOADLEVEL, GAMEOVER }
-    State _state;
-
-    public GameObject _currentBall;
-    public GameObject _currentLevel;
-    public bool _isSwitchingState;
-
-    public static GameManager Instance { get; private set; }
-
-    private int _score;
-
-    public int Score
+    public class GameManager : MonoBehaviour
     {
-        get { return _score; }
-        set
+        public GameObject ballPrefab;
+        public GameObject playerPrefab;
+
+        public Text scoreText;
+        public Text ballsText;
+        public Text levelText;
+        public Text highScoreText;
+
+        public GameObject panelMenu;
+        public GameObject panelPlay;
+        public GameObject panelLevelCompleted;
+        public GameObject panelGameOver;
+
+        public GameObject[] levels;
+
+        public enum State { MENU, INIT, PLAY, LEVELCOMPLETED, LOADLEVEL, GAMEOVER }
+        State _state;
+
+        public GameObject _currentBall;
+        public GameObject _currentLevel;
+        public bool _isSwitchingState;
+
+        public static GameManager Instance { get; private set; }
+
+        private int _score;
+
+        public int Score
         {
-            _score = value;
-            scoreText.text = "Score: " + Score + "(HighScore:" + PlayerPrefs.GetInt("highscore") + ")";
+            get => _score;
+            set
+            {
+                _score = value;
+                scoreText.text = "Score: " + Score + "(HighScore:" + PlayerPrefs.GetInt("highScore") + ")";
+            }
         }
-    }
 
-    private int _level;
+        private int _level;
 
-    public int Level
-    {
-        get { return _level; }
-        set
+        public int Level
         {
-            _level = value;
-            levelText.text = "Level: " + Level;
+            get => _level;
+            set
+            {
+                _level = value;
+                levelText.text = "Level: " + Level;
+            }
         }
-    }
 
-    private int _balls;
+        private int _balls;
 
-    public int Balls
-    {
-        get { return _balls; }
-        set
+        public int Balls
         {
-            _balls = value;
-            ballsText.text = "Balls: " + Balls;
+            get => _balls;
+            set
+            {
+                _balls = value;
+                ballsText.text = "Balls: " + Balls;
+            }
         }
-    }
 
-
-    void Start()
-    {
-        Instance = this;
-        SwitchState(State.MENU);
-    }
-
-    public void PlayClicked()
-    {
-        SwitchState(State.INIT);
-    }
-
-    public void SwitchState(State newState, float delay = 0)
-    {
-        StartCoroutine(SwitchDelay(newState, delay));
-    }
-
-    IEnumerator SwitchDelay(State newState, float delay)
-    {
-        _isSwitchingState = true;
-        yield return new WaitForSeconds(delay);
-        EndState();
-        _state = newState;
-        StartState(newState);
-        _isSwitchingState = false;
-    }
-
-    void StartState(State newState)
-    {
-        switch (newState)
+        private void Start()
         {
-            case State.MENU:
-                Cursor.visible = true;
-                panelMenu.SetActive(true);
-                break;
-            case State.INIT:
-                Cursor.visible = false;
-                panelPlay.SetActive(true);
-                Score = 0;
-                Level = 0;
-                Balls = 3;
-                if(_currentLevel != null)
-                {
-                    Destroy(_currentLevel);
-                }
-                Instantiate(playerPrefab);
-                SwitchState(State.LOADLEVEL);
-                break;
-            case State.PLAY:
-                break;
-            case State.LEVELCOMPLETED:
-                Destroy(_currentBall);
-                Destroy(_currentLevel);
-                Level++;
-                panelLevelCompleted.SetActive(true);
-                SwitchState(State.LOADLEVEL, 2f);
-                break;
-            case State.LOADLEVEL:
-                if (Level >= levels.Length)
-                {
-                    SwitchState(State.GAMEOVER);
-                }
-                else
-                {
-                    _currentLevel = Instantiate(levels[Level]);
-                    SwitchState(State.PLAY);
-                }
-                break;
-            case State.GAMEOVER:
-                if (Score > PlayerPrefs.GetInt("highscore"))
-                {
-                    PlayerPrefs.SetInt("highscore", Score);
-                }
-                panelGameOver.SetActive(true);
-                break;
+            Instance = this;
+            SwitchState(State.MENU);
         }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        switch (_state)
+
+        public void PlayClicked()
         {
-            case State.MENU:
-                break;
-            case State.INIT:
-                break;
-            case State.PLAY:
-                if (_currentBall == null)
-                {
-                    if (Balls > 0)
+            SwitchState(State.INIT);
+        }
+
+        public void SwitchState(State newState, float delay = 0)
+        {
+            StartCoroutine(SwitchDelay(newState, delay));
+        }
+
+        private IEnumerator SwitchDelay(State newState, float delay)
+        {
+            _isSwitchingState = true;
+            yield return new WaitForSeconds(delay);
+            EndState();
+            _state = newState;
+            StartState(newState);
+            _isSwitchingState = false;
+        }
+
+        private void StartState(State newState)
+        {
+            switch (newState)
+            {
+                case State.MENU:
+                    Cursor.visible = true;
+                    panelMenu.SetActive(true);
+                    break;
+                case State.INIT:
+                    Cursor.visible = false;
+                    panelPlay.SetActive(true);
+                    Score = 0;
+                    Level = 0;
+                    Balls = 3;
+                    if (_currentLevel != null)
                     {
-                        _currentBall = Instantiate(ballPrefab);
+                        Destroy(_currentLevel);
                     }
-                    else
+                    Instantiate(playerPrefab);
+                    SwitchState(State.LOADLEVEL);
+                    break;
+                case State.PLAY:
+                    break;
+                case State.LEVELCOMPLETED:
+                    Destroy(_currentBall);
+                    Destroy(_currentLevel);
+                    Level++;
+                    panelLevelCompleted.SetActive(true);
+                    SwitchState(State.LOADLEVEL, 2f);
+                    break;
+                case State.LOADLEVEL:
+                    if (Level >= levels.Length)
                     {
                         SwitchState(State.GAMEOVER);
                     }
-                }
-                if (_currentLevel != null && _currentLevel.transform.childCount == 0 && !_isSwitchingState)
-                {
-                    SwitchState(State.LEVELCOMPLETED);
-                }
-                break;
-            case State.LEVELCOMPLETED:
-                break;
-            case State.LOADLEVEL:
-                break;
-            case State.GAMEOVER:
-                if (Input.anyKeyDown)
-                {
-                    SwitchState(State.MENU);
-                }
-                break;
+                    else
+                    {
+                        _currentLevel = Instantiate(levels[Level]);
+                        SwitchState(State.PLAY);
+                    }
+                    break;
+                case State.GAMEOVER:
+                    if (Score > PlayerPrefs.GetInt("highScore"))
+                    {
+                        PlayerPrefs.SetInt("highScore", Score);
+                    }
+                    panelGameOver.SetActive(true);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+            }
         }
-    }
 
-    void EndState()
-    {
-        switch (_state)
+        // Update is called once per frame
+        private void Update()
         {
-            case State.MENU:
-                panelMenu.SetActive(false);
-                break;
-            case State.INIT:
-                break;
-            case State.PLAY:
-                break;
-            case State.LEVELCOMPLETED:
-                panelLevelCompleted.SetActive(false);
-                break;
-            case State.LOADLEVEL:
-                break;
-            case State.GAMEOVER:
-                panelPlay.SetActive(false);
-                panelGameOver.SetActive(false);
-                break;
+            switch (_state)
+            {
+                case State.MENU:
+                    break;
+                case State.INIT:
+                    break;
+                case State.PLAY:
+                    if (_currentBall == null)
+                    {
+                        if (Balls > 0)
+                        {
+                            _currentBall = Instantiate(ballPrefab);
+                        }
+                        else
+                        {
+                            SwitchState(State.GAMEOVER);
+                        }
+                    }
+                    if (_currentLevel != null && _currentLevel.transform.childCount == 0 && !_isSwitchingState)
+                    {
+                        SwitchState(State.LEVELCOMPLETED);
+                    }
+                    break;
+                case State.LEVELCOMPLETED:
+                    break;
+                case State.LOADLEVEL:
+                    break;
+                case State.GAMEOVER:
+                    if (Input.anyKeyDown)
+                    {
+                        SwitchState(State.MENU);
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void EndState()
+        {
+            switch (_state)
+            {
+                case State.MENU:
+                    panelMenu.SetActive(false);
+                    break;
+                case State.INIT:
+                    break;
+                case State.PLAY:
+                    break;
+                case State.LEVELCOMPLETED:
+                    panelLevelCompleted.SetActive(false);
+                    break;
+                case State.LOADLEVEL:
+                    break;
+                case State.GAMEOVER:
+                    panelPlay.SetActive(false);
+                    panelGameOver.SetActive(false);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
